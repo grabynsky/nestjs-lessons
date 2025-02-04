6533147001a5f853cee8026088d73490d05ab767
Grabynsky Andrew <hrabynsky@gmail.com>
2025-02-04 15:36:32 +0200

    add docker

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { AppConfig, Config } from '../configs/config.type';
+import { UpdateUserReqDto } from './models/dto/req/update-user.req.dto';
 import { UserReqDto } from './models/dto/req/user.req.dto';
 import { UserResDto } from './models/dto/res/user.res.dto';
-import { UpdateUserReqDto } from './models/dto/req/update-user.req.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly configService: ConfigService<Config>) {}
+
   public async create(createUserDto: UserReqDto): Promise<UserResDto> {
+    const appConfig = this.configService.get<AppConfig>('app');
+    console.log(appConfig);
     return {} as UserResDto;
   }
 
